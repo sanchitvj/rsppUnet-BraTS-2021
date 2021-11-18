19b9e36cae60b1723bac32dfe55ff3def53d0930
thejineaswar <thejineash@gmail.com>
2021-11-18 23:51:34 +0530

    Updating SPP for multi level encoder input

--- src/model/3d_spatial_pyramid_pooling.py
+++ src/model/3d_spatial_pyramid_pooling.py
@@ -10,11 +10,12 @@ import torch.nn.functional as F
         Contains the size of the pooling kernel. In total 2 pooling layers present. Each element in the 
         list corresponds to filter size n * n. Ex : Element at 0 index is 1 hence first pooling layer will
         have 1 *1 pooling size
-        
+
     mode -> String : 'avg' or 'max'
         avg corresponds to average pooling
         max corresponds to max pooling
 """
+
 
 class Pyramid_Pooling_3D(nn.Module):
     def __init__(self, levels, mode="max"):
@@ -24,85 +25,55 @@ class Pyramid_Pooling_3D(nn.Module):
         :param mode   -> String : Decides whether to use max or average pooling. Should be either "max" or "avg"
         """
         super(Pyramid_Pooling_3D, self).__init__()
-        self.levels = levels
-        self.mode = mode
+        # self.levels = levels
+        assert len(levels) == 3
 
-    def forward(self, x):
-        return self.spatial_pyramid_pool(x, self.levels, self.mode)
+        if mode == 'max':
+            self.pool_1 = nn.MaxPool3d((levels[2], levels[2], levels[2]))
+            self.pool_2 = nn.MaxPool3d((levels[1], levels[1], levels[1]))
+            self.pool_3 = nn.MaxPool3d((levels[0], levels[0], levels[0]))
+        elif mode == 'avg':
+            self.pool_1 = nn.AvgPool3d((levels[2], levels[2], levels[2]))
+            self.pool_2 = nn.AvgPool3d((levels[1], levels[1], levels[1]))
+            self.pool_3 = nn.AvgPool3d((levels[0], levels[0], levels[0]))
 
-    def get_output_size(self, filters):
-        out = 0
-        for level in self.levels:
-            out += filters * level * level
-        return out
+    def forward(self, en1, en2, en3):
+        print(en1.shape)
+        pooled_out_1 = self.pool_1(en1)
+        pooled_out_2 = self.pool_2(en2)
+        pooled_out_3 = self.pool_3(en3)
+        cat = torch.cat((pooled_out_1, pooled_out_2, pooled_out_3), 1)
+        return cat
 
-    @staticmethod
-    def spatial_pyramid_pool(previous_conv, levels, mode):
-
-        """
-        """
-        num_sample = previous_conv.size(0)
-        num_channel = previous_conv.size(1)
-        previous_conv_size = [
-                                int(previous_conv.size(-3)),
-                                int(previous_conv.size(-2)),
-                                int(previous_conv.size(-1))
-                        ]
-
-        for i in range(len(levels)):
-            z_kernel = int(math.ceil(previous_conv_size[0] / levels[i]))
-            h_kernel = int(math.ceil(previous_conv_size[1] / levels[i]))
-            w_kernel = int(math.ceil(previous_conv_size[2] / levels[i]))
-
-            z_pad1 = int(math.floor((z_kernel * levels[i] - previous_conv_size[0]) / 2))
-            z_pad2 = int(math.ceil((z_kernel * levels[i] - previous_conv_size[0]) / 2))
-
-            h_pad1 = int(math.floor((h_kernel * levels[i] - previous_conv_size[1]) / 2))
-            h_pad2 = int(math.ceil((h_kernel * levels[i] - previous_conv_size[1]) / 2))
-
-            w_pad1 = int(math.floor((w_kernel * levels[i] - previous_conv_size[2]) / 2))
-            w_pad2 = int(math.ceil((w_kernel * levels[i] - previous_conv_size[2]) / 2))
-
-
-            assert  w_pad1 + w_pad2 == (w_kernel * levels[i] - previous_conv_size[2]) and \
-                    h_pad1 + h_pad2 == (h_kernel * levels[i] - previous_conv_size[1]) and \
-                    z_pad1 + z_pad2 == (z_kernel * levels[i] - previous_conv_size[0])
-
-
-            padded_input = F.pad(input=previous_conv, pad=[w_pad1, w_pad2, h_pad1, h_pad2,z_pad1,z_pad2],
-                                 mode='constant', value=0)
-            if mode == "max":
-                pool = nn.MaxPool3d((levels[i],levels[i],levels[i]))
-            elif mode == "avg":
-                pool = nn.AvgPool3d((z_kernel,h_kernel, w_kernel))
-            else:
-                raise RuntimeError("Unknown pooling type: %s, please use \"max\" or \"avg\".")
-            x = pool(padded_input)
-
-            dim = x.size(-1) **3
-            x = x.numpy()
-            by_factor = math.ceil(previous_conv_size[0]/levels[-1])
-            dim /= math.ceil(previous_conv_size[0]/levels[-1])**2
-
-            reshape_size = (num_sample, num_channel, math.ceil(dim), by_factor, by_factor)
-
-            x = np.resize(x,reshape_size)
-            if i == 0:
-                spp = x
-            else:
-                spp = np.concatenate((spp,x),2)
-        return spp
 
 if __name__ == '__main__':
-    x_train = np.random.randn(3, 1,64, 64, 64)
+    x_train = np.random.randn(3, 64, 16, 16, 16)
     x_train = torch.from_numpy(x_train).float()
-    pyramid_pooling = Pyramid_Pooling_3D([2,4,6] , 'max')
-    out = pyramid_pooling(x_train)
+
+    x_train_1 = np.random.randn(3, 128, 32, 32, 32)
+    x_train_1 = torch.from_numpy(x_train_1).float()
+
+    x_train_2 = np.random.randn(3, 256, 64, 64, 64)
+    x_train_2 = torch.from_numpy(x_train_2).float()
+
+    pyramid_pooling = Pyramid_Pooling_3D([2, 4, 8], 'max')
+    out = pyramid_pooling(x_train_2, x_train_1, x_train)
     print(f"Output  shape {out.shape}")
 
+"""
+torch.Size([3, 32, 128, 128, 128]) -> 512, 64, 64, 64 => 576
+torch.Size([3, 64, 64, 64, 64])    -> (3,64,4096,8,8
+torch.Size([3, 128, 32, 32, 32]) -> (3,128,512,8,8)
+torch.Size([3, 256, 16, 16, 16]) -> (3,256,64,8,8)
+torch.Size([3, 512, 8, 8, 8]) -> As it is Concatenate the other 2 
+"""
 
-
-
+"""
+Pooling size :
+    Fpr 64*64*64 -> Pool size 8
+    For 32*32*32 -> Pool size 4
+    For 16*16*16 -> Pool size 2
+"""
 
 
 
