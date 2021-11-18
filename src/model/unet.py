5b051c3d3ee56abf179f889feb478c2ec9648a8b
Sanchit <sanchit.vijay@bytelearn.ai>
2021-11-18 18:47:27 +0530

    model corrections

--- src/model/unet.py
+++ src/model/unet.py
@@ -269,7 +269,7 @@ class AttentionBlock(nn.Module):
         relu = self.relu(g1 + x1)
         sig = self.psi(relu)
 
-        ################ DOUBT ###############
+        ################ Modifications possible ###############
 
         alpha = nn.functional.interpolate(
             sig, scale_factor=self.scale_factor, mode=self.mode
@@ -279,152 +279,6 @@ class AttentionBlock(nn.Module):
             return alpha
         else:
             return x * alpha
-
-
-# class VDResampling(nn.Module):
-#     """
-#     Variational Auto-Encoder Resampling block
-#     """
-
-#     def __init__(
-#         self,
-#         inChans=256,
-#         outChans=256,
-#         dense_features=(10, 12, 8),
-#         stride=2,
-#         kernel_size=3,
-#         padding=1,
-#         activation="relu",
-#         normalizaiton="group_normalization",
-#     ):
-#         super(VDResampling, self).__init__()
-
-#         midChans = int(inChans / 2)
-#         self.dense_features = dense_features
-#         if normalizaiton == "group_normalization":
-#             self.gn1 = nn.GroupNorm(num_groups=8, num_channels=inChans)
-#         if activation == "relu":
-#             self.actv1 = nn.ReLU(inplace=True)
-#             self.actv2 = nn.ReLU(inplace=True)
-#         elif activation == "elu":
-#             self.actv1 = nn.ELU(inplace=True)
-#             self.actv2 = nn.ELU(inplace=True)
-#         self.conv1 = nn.Conv3d(
-#             in_channels=inChans,
-#             out_channels=16,
-#             kernel_size=kernel_size,
-#             stride=stride,
-#             padding=padding,
-#         )
-#         self.dense1 = nn.Linear(
-#             in_features=16 * dense_features[0] * dense_features[1] * dense_features[2],
-#             out_features=inChans,
-#         )
-#         self.dense2 = nn.Linear(
-#             in_features=midChans,
-#             out_features=midChans
-#             * dense_features[0]
-#             * dense_features[1]
-#             * dense_features[2],
-#         )
-#         self.up0 = LinearUpSampling(midChans, outChans)
-
-#     def forward(self, x):
-#         out = self.gn1(x)
-#         out = self.actv1(out)
-#         out = self.conv1(out)
-#         out = out.view(-1, self.num_flat_features(out))
-#         out_vd = self.dense1(out)
-#         distr = out_vd
-#         out = VDraw(out_vd)
-#         out = self.dense2(out)
-#         out = self.actv2(out)
-#         out = out.view(
-#             (
-#                 1,
-#                 128,
-#                 self.dense_features[0],
-#                 self.dense_features[1],
-#                 self.dense_features[2],
-#             )
-#         )
-#         out = self.up0(out)
-
-#         return out, distr
-
-#     def num_flat_features(self, x):
-#         size = x.size()[1:]
-#         num_features = 1
-#         for s in size:
-#             num_features *= s
-
-#         return num_features
-
-
-# def VDraw(x):
-#     # Generate a Gaussian distribution with the given mean(128-d) and std(128-d)
-#     return torch.distributions.Normal(x[:, :128], x[:, 128:]).sample()
-
-
-# class VDecoderBlock(nn.Module):
-#     """
-#     Variational Decoder block
-#     """
-
-#     def __init__(
-#         self,
-#         inChans,
-#         outChans,
-#         activation="relu",
-#         normalizaiton="group_normalization",
-#         mode="trilinear",
-#     ):
-#         super(VDecoderBlock, self).__init__()
-
-#         self.up0 = LinearUpSampling(inChans, outChans, mode=mode)
-#         self.block = DecoderBlock(
-#             outChans, outChans, activation=activation, normalizaiton=normalizaiton
-#         )
-
-#     def forward(self, x):
-#         out = self.up0(x)
-#         out = self.block(out)
-
-#         return out
-
-
-# class VAE(nn.Module):
-#     """
-#     Variational Auto-Encoder : to group the features extracted by Encoder
-#     """
-
-#     def __init__(
-#         self,
-#         inChans=256,
-#         outChans=4,
-#         dense_features=(10, 12, 8),
-#         activation="relu",
-#         normalizaiton="group_normalization",
-#         mode="trilinear",
-#     ):
-#         super(VAE, self).__init__()
-
-#         self.vd_resample = VDResampling(
-#             inChans=inChans, outChans=inChans, dense_features=dense_features
-#         )
-#         self.vd_block2 = VDecoderBlock(inChans, inChans // 2)
-#         self.vd_block1 = VDecoderBlock(inChans // 2, inChans // 4)
-#         self.vd_block0 = VDecoderBlock(inChans // 4, inChans // 8)
-#         self.vd_end = nn.Conv3d(inChans // 8, outChans, kernel_size=1)
-
-#     def forward(self, x):
-#         out, distr = self.vd_resample(x)
-#         out = self.vd_block2(out)
-#         out = self.vd_block1(out)
-#         out = self.vd_block0(out)
-#         out = self.vd_end(out)
-
-#         return out, distr
 
 
 class NvNet(nn.Module):
@@ -464,77 +318,71 @@ class NvNet(nn.Module):
             128, 128, activation=self.activation, normalizaiton=self.normalizaiton
         )
 
-        self.en_down2 = DownSampling(128, 256)
-        self.en_block2_0 = EncoderBlock(
+        self.en_down3 = DownSampling(128, 256)
+        self.en_block3_0 = EncoderBlock(
             256, 256, activation=self.activation, normalizaiton=self.normalizaiton
         )
-        self.en_block2_1 = EncoderBlock(
+        self.en_block3_1 = EncoderBlock(
             256, 256, activation=self.activation, normalizaiton=self.normalizaiton
         )
 
-        self.en_down3 = DownSampling(256, 320)
-        self.en_block3_0 = EncoderBlock(
+        self.en_down4 = DownSampling(256, 320)
+        self.en_block4_0 = EncoderBlock(
             320, 320, activation=self.activation, normalizaiton=self.normalizaiton
         )
-        self.en_block3_1 = EncoderBlock(
+        self.en_block4_1 = EncoderBlock(
             320, 320, activation=self.activation, normalizaiton=self.normalizaiton
         )
-        self.en_block3_2 = EncoderBlock(
+        self.en_block4_2 = EncoderBlock(
             320, 320, activation=self.activation, normalizaiton=self.normalizaiton
         )
-        self.en_block3_3 = EncoderBlock(
+        self.en_block4_3 = EncoderBlock(
             320, 320, activation=self.activation, normalizaiton=self.normalizaiton
         )
 
         ######################  SPATIAL PYRAMID POOLING BLOCK   ###########################
 
         # Decoder Blocks
-        self.de_up2 = Deconvolution(320, 256, mode=self.mode)
-        self.de_block2 = DecoderBlock(
+        self.de_up3 = Deconvolution(320, 256, mode=self.mode)
+        self.de_block3 = DecoderBlock(
             256, 256, activation=self.activation, normalizaiton=self.normalizaiton
         )
-        self.de_up1 = Deconvolution(256, 128, mode=self.mode)
-        self.de_block1 = DecoderBlock(
+        self.de_up2 = Deconvolution(256, 128, mode=self.mode)
+        self.de_block2 = DecoderBlock(
             128, 128, activation=self.activation, normalizaiton=self.normalizaiton
         )
-        self.de_up0 = Deconvolution(128, 64, mode=self.mode)
-        self.de_block0 = DecoderBlock(
+        self.de_up1 = Deconvolution(128, 64, mode=self.mode)
+        self.de_block1 = DecoderBlock(
             64, 64, activation=self.activation, normalizaiton=self.normalizaiton
         )
         self.de_up0 = Deconvolution(64, 32, mode=self.mode)
         self.de_block0 = DecoderBlock(
             32, 32, activation=self.activation, normalizaiton=self.normalizaiton
         )
-        self.de_end = OutputTransition(32, self.seg_outChans)
 
         # Attention Blocks
+        self.ag_3 = AttentionBlock(320, 256, 256)
         self.ag_2 = AttentionBlock(256, 128, 128)  # forward(g, x)
         self.ag_1 = AttentionBlock(128, 64, 64)
         self.ag_0 = AttentionBlock(64, 32, 32)
 
-        # Variational Auto-Encoder
-        # if self.config["VAE_enable"]:
-        #     self.dense_features = (self.input_shape[2]//16, self.input_shape[3]//16, self.input_shape[4]//16)
-        #     self.vae = VAE(256, outChans=self.inChans - 3, dense_features=self.dense_features)   ### 7 ——> 4
+        self.de_end = OutputTransition(32, self.seg_outChans)
 
     def forward(self, x):
         out_init = self.in_conv0(x)  # (7, 128, 192, 160)
         out_en0 = self.en_block0(out_init)
         out_en1 = self.en_block1_1(self.en_block1_0(self.en_down1(out_en0)))
         out_en2 = self.en_block2_1(self.en_block2_0(self.en_down2(out_en1)))
-        out_en3 = self.en_block3_3(
-            self.en_block3_2(self.en_block3_1(self.en_block3_0(self.en_down3(out_en2))))
+        out_en3 = self.en_block3_1(self.en_block3_0(self.en_down3(out_en2)))
+        out_en4 = self.en_block4_3(
+            self.en_block4_2(self.en_block4_1(self.en_block4_0(self.en_down4(out_en3))))
         )
 
-        out_de2 = self.de_block2(self.de_up2(out_en3, self.ag_2(out_en3, out_en2)))
+        out_de3 = self.de_block3(self.de_up3(out_en4, self.ag_3(out_en4, out_en3)))
+        out_de2 = self.de_block2(self.de_up2(out_de3, self.ag_2(out_de3, out_en2)))
         out_de1 = self.de_block1(self.de_up1(out_de2, self.ag_1(out_de2, out_en1)))
         out_de0 = self.de_block0(self.de_up0(out_de1, self.ag_0(out_de1, out_en0)))
 
         out_end = self.de_end(out_de0)
-
-        # if self.config["VAE_enable"]:
-        #     out_vae, out_distr = self.vae(out_en3)
-        #     out_final = torch.cat((out_end, out_vae), 1)
-        #     return out_final, out_distr
 
         return out_end
